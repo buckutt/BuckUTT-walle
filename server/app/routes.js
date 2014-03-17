7fbc6cd5a2db504b6202c4528b8287370a08dd24
PymZoR <pymzorr@gmail.com>
2014-03-17 18:20:50 +0100

    Add /api/point in the routes

--- server/app/routes.js
+++ server/app/routes.js
@@ -2,8 +2,10 @@ module.exports = function(app, dbConnection){
 	var users = require('./api/users/users.js');
     var products = require('./api/products/products.js');
     var purchases = require('./api/purchases/purchases.js');
-  
+    var points = require('./api/points/points.js');
+
     users.users(app, dbConnection);
     products.products(app, dbConnection);
 	purchases.purchases(app, dbConnection, users, products);
+    points.points(app, dbConnection);
 }
