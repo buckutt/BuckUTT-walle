a5b3bb787a4cb000de6365fb39063ed4324c9050
PymZoR <pymzorr@gmail.com>
2014-03-18 20:52:21 +0100

    Forgot to add GET /api/points

---
+++ server/app/api/points/points.js
@@ -0,0 +1,31 @@
+var points = module.exports;
+
+var dependency = null;
+
+
+/*
+    Return all the sell points
+    Take a callback function to handle data  
+*/
+
+points.getPointList = function(handleData){
+    var query = "SELECT poi_id, poi_name FROM t_point_poi";
+
+    dependency.dbConnection.query(query, null, function(err, rows, fields){
+        if(err) throw err;
+        handleData(rows);
+    });
+}
+
+
+points.points = function(container){
+    dependency = container;
+
+    dependency.app.get("/api/points/", function(req, res){
+        points.getPointList(function(data){
+            res.json(data);
+        });
+    })
+
+    ;
+}
