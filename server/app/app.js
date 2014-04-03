ae561f19583faeff653700c8c4254433a2f6f65e
Axel Mousset <pymzorr@gmail.com>
2014-04-03 11:18:58 +0000

    Bugfix 404 error on all API

--- server/app/app.js
+++ server/app/app.js
@@ -43,11 +43,12 @@ dependency.app = express()
 .use(express.json())
 .use(express.static(path.join(__dirname, '../../client/src')));
 
+require('./routes.js')(dependency);
+
+//404 error
 dependency.app.use(function(req, res, next){
     res.sendfile(path.resolve('client/src/assets/img/404.png'));
 });
-
-require('./routes.js')(dependency);
 
 dependency.app.listen(8081);
 console.log("Listening on 8081");
